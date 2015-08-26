f233ed622534aeddb2d1cf9bf9790e0bed1535d6
Brian Graham <bcgraham+github@gmail.com>
2015-08-25 23:08:06 -0400

    Start cron from entrypoint.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 export PGDATA=/var/lib/postgresql/data
 export PGBIN=/usr/lib/postgresql/9.4/bin
 
+service cron start
 /usr/local/apache2/bin/httpd -d /terra-mystica/www-docker -f ../config/apache.conf 
-
 su postgres -c "$PGBIN/postgres -o \"-c listen_addresses=''\""
